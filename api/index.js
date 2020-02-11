1e884857cf32c3e65dcf983870af49c7b6516586
nicolassalvanes <nicolas.salvanes@mercadolibre.com>
2020-02-11 11:34:39 -0300

    Agrego config por archivo y comentarios

--- api/index.js
+++ api/index.js
@@ -2,24 +2,32 @@ const router = require('express').Router();
 const request = require('request');
 const uuidv1 = require('uuid/v1');
 const util = require('util');
+const propertiesReader = require('properties-reader');
+const properties = propertiesReader('./api/config/properties.conf');
 
+/**
+ * In-memory "database" for storing each order status by his external reference id
+ * (Real database implementation required on production)
+ */
 var db = [];
 
-const ACCESS_TOKEN = 'TEST-1783827438008150-020716-8ff29f683360bea4930c7e5950c54b16-523903831',
-      USER_ID = '523903831',
-      POS_ID = 'default',
-      MP_ORDER_URL = 'https://api.mercadopago.com/mpmobile/instore/qr/'+USER_ID+'/'+POS_ID+'?access_token='+ACCESS_TOKEN,
-      NOTIFICATION_CALLBACK_URL = 'https://workshopqr.requestcatcher.com/test',
-      MP_MERCHANT_URL = 'https://api.mercadopago.com/merchant_orders/%d?access_token='+ACCESS_TOKEN;
+const ACCESS_TOKEN = properties.get('access_token'),
+      USER_ID = properties.get('user_id'),
+      POS_ID = properties.get('pos_id'),
+      NOTIFICATION_CALLBACK_URL = properties.get('notification_callback_url'),
+      MP_ORDER_URL = properties.get('mp_order_basepath')+USER_ID+'/'+POS_ID+'?access_token='+ACCESS_TOKEN,
+      MP_MERCHANT_URL = properties.get('mp_merchant_basepath')+'%d?access_token='+ACCESS_TOKEN;
 
-      
+/**
+ * This resource creates an instore order for item payment
+ */
 router.post('/order', (req, res) => {
     
     var externalReference = POS_ID+'-'+uuidv1();
 
     var options = {
-		uri: MP_ORDER_URL,
-		method: "POST",
+        uri: MP_ORDER_URL,
+        method: "POST",
         json: true,
         body: {
             "notification_url": NOTIFICATION_CALLBACK_URL,
@@ -33,11 +41,11 @@ router.post('/order', (req, res) => {
         }
     }
 	
-	request(options, function(err, response, body) {
+    request(options, function(err, response, body) {
 
-		if (err || response.statusCode !== 200) {
+        if (err || response.statusCode !== 200) {
             return res.status(500).json({ "error": err });
-            
+
         } else {
             db[externalReference] = 'unknown';
 
@@ -48,22 +56,23 @@ router.post('/order', (req, res) => {
     });
 });
 
+/**
+ * This resource receives MP notifications for each order update, then retrieve and store its current status. 
+ */
 router.post('/notification', (req, res) => {
-
     if(req.query.topic === 'merchant_order'){
-
         var id = req.query.id;
-    
+
         var options = {
             uri: util.format(MP_MERCHANT_URL, id),
             method: "GET"
         }
-        
+
         request(options, function(err, response, body) {
 
             if (err || response.statusCode !== 200) {
                 console.log(err);
-                
+
             } else {
                 var order = JSON.parse(response.body);
                 db[order.external_reference] = order.status;
@@ -73,6 +82,9 @@ router.post('/notification', (req, res) => {
     return res.sendStatus(200);
 });
 
+/**
+ * This resource returns an order last known status
+ */
 router.get('/status', (req, res) => {
     var externalReference = req.query.external_reference;
 
--- app/index.js
+++ app/index.js
@@ -2,19 +2,26 @@ const router = require('express').Router();
 
 const request = require('request');
 
+/**
+ * Store home page: displays all available items.
+ */
 router.get('/', function (req, res) {
     res.render('home');
 });
 
+/**
+ * Item detail page: requests order creation for selected item, 
+ * and displays payment info with current order status
+ */
 router.post('/detail', function (req, res) {
 
     const api_baseurl = req.protocol+'://'+req.get('host')+'/api';
-    
+
     var data = req.body;
 
     var options = {
-		uri: api_baseurl+'/order',
-		method: "POST",
+        uri: api_baseurl+'/order',
+        method: "POST",
         json: true,
         body: {
             "title": data.title,
@@ -23,11 +30,11 @@ router.post('/detail', function (req, res) {
         }
     }
 	
-	request(options, function(err, response, body) {
+    request(options, function(err, response, body) {
 
-		if (err || response.statusCode !== 200) {
+        if (err || response.statusCode !== 200) {
             return res.status(500).json({ "error": err });
-            
+
         } else {
             data.external_reference = response.body.order.external_reference;
             data.api_baseurl = api_baseurl;
